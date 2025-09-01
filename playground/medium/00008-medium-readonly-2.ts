640ebfdd1b369a95b43aa7867cff51506f452f57
devonyou <onyou.code@gmail.com>
2025-09-01 22:25:27 +0900

    8 - Readonly 2

--- playground/medium/00008-medium-readonly-2.ts
+++ playground/medium/00008-medium-readonly-2.ts
@@ -5,7 +5,8 @@
 
   ### 질문
 
-  `T`에서 `K` 프로퍼티만 읽기 전용으로 설정해 새로운 오브젝트 타입을 만드는 제네릭 `MyReadonly2<T, K>`를 구현하세요. `K`가 주어지지 않으면 단순히 `Readonly<T>`처럼 모든 프로퍼티를 읽기 전용으로 설정해야 합니다.
+  `T`에서 `K` 프로퍼티만 읽기 전용으로 설정해 새로운 오브젝트 타입을 만드는 제네릭 `MyReadonly2<T, K>`를 구현하세요.
+  `K`가 주어지지 않으면 단순히 `Readonly<T>`처럼 모든 프로퍼티를 읽기 전용으로 설정해야 합니다.
 
   예시:
 
@@ -32,7 +33,11 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
-type MyReadonly2<T, K> = any
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+  readonly [p in keyof T as p extends K ? p : never]: T[p];
+} & {
+  [p in keyof T as p extends K ? never : p]: T[p];
+}
 
 /* _____________ 테스트 케이스 _____________ */
 import type { Alike, Expect } from '@type-challenges/utils'
