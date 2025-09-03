b6d67d62b9925bc724fe266227c2e95bbb4453ba
devonyou <onyou.code@gmail.com>
2025-09-03 18:57:09 +0900

    119 - ReplaceAll

--- playground/medium/00119-medium-replaceall.ts
+++ playground/medium/00119-medium-replaceall.ts
@@ -5,7 +5,8 @@
 
   ### 질문
 
-  주어진 문자열 `S`에서 부분 문자열 `From`을 찾아 모두 `To`로 교체하는 제네릭 `ReplaceAll<S, From, To>`을 구현하세요.
+  주어진 문자열 `S`에서 부분 문자열 `From`을 찾아 모두 `To`로 교체하는 제네릭
+  `ReplaceAll<S, From, To>`을 구현하세요.
 
   예시:
 
@@ -18,7 +19,12 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
-type ReplaceAll<S extends string, From extends string, To extends string> = any
+type ReplaceAll<S extends string, From extends string, To extends string> =
+  From extends ''
+    ? S :
+    S extends `${infer R}${From}${infer Rest}`
+      ? `${R}${To}${ReplaceAll<Rest, From, To>}`
+      : S
 
 /* _____________ 테스트 케이스 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
