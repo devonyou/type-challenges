f11a41186a5205171516fe126033309cfb1ea478
devonyou <onyou.code@gmail.com>
2025-09-05 08:21:12 +0900

    531 - String to Union

--- playground/medium/00531-medium-string-to-union.ts
+++ playground/medium/00531-medium-string-to-union.ts
@@ -20,7 +20,8 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
-type StringToUnion<T extends string> = any
+type StringToUnion<T extends string> =
+  `${T}` extends `${infer R}${infer Rest}` ? R | StringToUnion<Rest> : never
 
 /* _____________ 테스트 케이스 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
